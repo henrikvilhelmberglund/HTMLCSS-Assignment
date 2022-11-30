e57c56de84d9b1771eeb91e1faa4c933f63684dd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-30 23:35:51 +0100

    feat: :tada: test to confirm that SCSS and tailwind are working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="nicediv">
+  <h1>Welcome to SvelteKit</h1>
+  <p class="bg-emerald-500">
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</div>
+
+<style lang="scss">
+  .nicediv {
+    h1 {
+      color: red;
+    }
+  }
+</style>
