38c6072974d57fdbd8477d60a0f282132962236a
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-01 11:30:03 +0100

    feat: :sparkles: add static adapter (needed for github pages), error page, svelte inspector feature

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<h1>{$page.status}: {$page.error.message}</h1>
+
+{#if $page.status === 404}
+  <p>The duck you're looking for isn't in here.</p>
+{:else}
+  <p>Unknown error.</p>
+{/if}
+
+<style>
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
+
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: dev ? "" : "/HTMLCSS-Assignment",
+    },
+  },
+  vitePlugin: {
+    experimental: { inspector: true },
   },
 
   preprocess: [
