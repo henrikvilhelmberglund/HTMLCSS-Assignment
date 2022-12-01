ed096dc244e45d39245063f2a034b590dda0f755
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-01 10:43:14 +0100

    feat: :sparkles: add nav and links to the main layout (will be visible in all pages)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script>
   import "../app.scss";
   import "../app.postcss";
+  import { page } from "$app/stores";
+  import AHeader from "$lib/AHeader.svelte"
 </script>
 
+<svelte:head>
+  {#if $page.url.pathname==="/"}
+  <title>Ankademin Cafe</title>
+  {:else}
+  <title>a</title>
+  {/if}
+</svelte:head>
+
+<nav>
+  <AHeader href="/">Home</AHeader>
+  <AHeader href="/menu">Menu</AHeader>
+  <AHeader href="/contact">Contact</AHeader>
+  <AHeader href="/reviews">Reviews</AHeader>
+  <AHeader href="/about">About</AHeader>
+</nav>
 <slot />
