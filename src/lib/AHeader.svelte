52e5a7630576f9ef741c6d843017e9ec8f572c57
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-02 16:56:19 +0100

    feat: :art: update AHeader hover/active style

--- src/lib/AHeader.svelte
+++ src/lib/AHeader.svelte
@@ -7,11 +7,14 @@
 {#if thisPage === $page.url.pathname.replaceAll("/", "")}
   <a
     {href}
-    class="text-base hover:text-emerald-600 text-emerald-600 underline decoration-emerald-600">
+    class="p-1 text-base text-white rounded-md border-2 border-solid border-emerald-700 bg-emerald-500">
     <slot />
   </a>
 {:else}
-  <a {href} class="text-base hover:text-emerald-600">
+  <a
+    {href}
+    class="p-1 text-base rounded-md border-2 border-solid border-slate-300
+    hover:border-emerald-500 ">
     <slot />
   </a>
 {/if}
