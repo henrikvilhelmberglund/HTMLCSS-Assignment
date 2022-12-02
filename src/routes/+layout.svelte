50fa9832d4aeaa3777cba0b2d6c1bf1c755f8796
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-02 17:55:42 +0100

    feat: :sparkles: put header in a header tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,22 @@
   import { page } from "$app/stores";
   import { base } from "$app/paths";
   import AHeader from "$lib/AHeader.svelte";
+  import Logo from "$lib/Logo.svelte";
 </script>
 
 <svelte:head>
   <title>{$page.data.title}</title>
 </svelte:head>
 
-<nav class="flex justify-center gap-4">
-  <AHeader thisPage="" href="{base}/">Home</AHeader>
-  <AHeader thisPage="menu" href="{base}/menu">Menu</AHeader>
-  <AHeader thisPage="contact" href="{base}/contact">Contact</AHeader>
-  <AHeader thisPage="reviews" href="{base}/reviews">Reviews</AHeader>
-  <AHeader thisPage="about-us" href="{base}/about-us">About us</AHeader>
-</nav>
+<header>
+  <Logo />
+
+  <nav class="flex justify-center gap-4">
+    <AHeader thisPage="" href="{base}/">Home</AHeader>
+    <AHeader thisPage="menu" href="{base}/menu">Menu</AHeader>
+    <AHeader thisPage="contact" href="{base}/contact">Contact</AHeader>
+    <AHeader thisPage="reviews" href="{base}/reviews">Reviews</AHeader>
+    <AHeader thisPage="about-us" href="{base}/about-us">About us</AHeader>
+  </nav>
+</header>
 <slot />
