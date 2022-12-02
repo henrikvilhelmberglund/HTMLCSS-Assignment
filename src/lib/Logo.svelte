d100a8bcf7db2afb795b9f5fca413e30f06f21fc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-02 17:55:22 +0100

    feat: :sparkles: add logo component

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "@fontsource/cherry-swash";
+</script>
+
+<p class="bg-yellow-200 p-2 font-logo text-4xl text-center bg-gradient-to-b to-yellow-50 from-yellow-300">Ankademin Cafe</p>
+
+<style>
+</style>
