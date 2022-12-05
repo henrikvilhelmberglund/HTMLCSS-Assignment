c0cd402852281728976f147f6c86612680e9caa2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 17:52:45 +0100

    feat: :sparkles: add image to main index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script>
   import P from "$lib/P.svelte";
   import Motto from "$lib/Motto.svelte";
+  import { base } from "$app/paths";
 </script>
 
-<main class="flex flex-col items-center justify-center h-[80vh]">
+<main class="flex flex-col items-center justify-between h-[70vh] bg-emerald-900">
+  <img
+    src="{base}/images/ankademin-cafe/ankademin_cafe_interior.jpg"
+    alt="Inside of Ankademin Cafe" />
   <Motto />
 
-  <P>
+  <P type="index">
     Ankademin Cafe is a duck cafe where you can have a great time mingling with
     our ducks or enjoy some Swedish fika.
   </P>
-  <P>We provide the finest ducks and the tastiest cookies. Welcome!</P>
+  <P type="index"
+    >We provide the finest ducks and the tastiest cookies. Welcome!</P>
 </main>
 
 <style>
