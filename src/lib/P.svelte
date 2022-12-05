9739f953130e16ce64602158d806b06a234d77c8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 20:28:37 +0100

    fix: :bug: remove doubled borders

--- src/lib/P.svelte
+++ src/lib/P.svelte
@@ -3,7 +3,7 @@
 </script>
 
 {#if type === "index"}
-  <p class="p-4 bg-emerald-800 text-emerald-200 border-y border-solid border-black">
+  <p class="p-4 bg-emerald-800 text-emerald-200 border-b border-solid border-black">
     <slot />
   </p>
 {:else if type === "other"}
