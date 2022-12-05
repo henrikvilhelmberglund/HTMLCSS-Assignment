549139ac5e56cf10ec334d40f0297639bfa52b01
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 17:50:11 +0100

    feat: :sparkles: add footer with links, logo and copyright info

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,9 +1,21 @@
 <script>
+  import A from "$lib/A.svelte";
+  import { base } from "$app/paths";
 </script>
 
-<p>Links</p>
-<p>Logo</p>
-<p>Copyright</p>
+<footer class="bg-slate-700">
+  <p class="p-2 font-logo text-3xl text-center text-white">Ankademin Cafe</p>
+  <nav aria-label="secondary-navigation" class="flex justify-center gap-4">
+    <A href="{base}/">Home</A>
+    <A href="{base}/menu">Menu</A>
+    <A href="{base}/contact">Contact</A>
+    <A href="{base}/reviews">Reviews</A>
+    <A href="{base}/about-us">About us</A>
+  </nav>
+
+  <small class="text-white text-right block"
+    >&copy; Copyright 2022 Ankademin Cafe</small>
+</footer>
 
 <style>
 </style>
