39dcbc4905305e399af82b1546c917034c035fa2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 17:51:30 +0100

    feat: :art: change gradient color in Logo component

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -2,7 +2,7 @@
   import "@fontsource/cherry-swash";
 </script>
 
-<p class="bg-yellow-200 p-2 font-logo text-4xl text-center bg-gradient-to-b to-yellow-50 from-yellow-300">Ankademin Cafe</p>
+<p class="p-2 font-logo text-4xl text-center bg-gradient-to-b to-orange-50 from-orange-400">Ankademin Cafe</p>
 
 <style>
 </style>
