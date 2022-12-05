d7149a22a0982db03c8c034f2f5bfc010978358e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 17:49:47 +0100

    feat: :sparkles: add another A component

---
+++ src/lib/A.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from "$app/stores";
+  export let href;
+</script>
+
+{#if href === $page.url.pathname}
+  <a
+    {href}
+    class="p-1 text-base text-white rounded-md border-2 border-solid border-emerald-700 bg-emerald-500">
+    <slot />
+  </a>
+{:else}
+  <a
+    {href}
+    class="p-1 text-base text-black rounded-md border-2 border-solid border-slate-600
+    hover:border-emerald-500 ">
+    <slot />
+  </a>
+{/if}
