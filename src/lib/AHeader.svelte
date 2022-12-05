5b9f3f5aa4ad9b0545a2d7292d254c99d7a57841
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 16:38:09 +0100

    feat: :fire: use href prop instead of thisPage

--- src/lib/AHeader.svelte
+++ src/lib/AHeader.svelte
@@ -1,10 +1,9 @@
 <script>
   import { page } from "$app/stores";
   export let href;
-  export let thisPage;
 </script>
 
-{#if thisPage === $page.url.pathname.replaceAll("/", "")}
+{#if href === $page.url.pathname}
   <a
     {href}
     class="p-1 text-base text-white rounded-md border-2 border-solid border-emerald-700 bg-emerald-500">
