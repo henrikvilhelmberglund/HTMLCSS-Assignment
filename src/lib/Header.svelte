025b8f63ae9a76a6ed74c947bae293b9b88b0e4c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-05 17:50:35 +0100

    feat: :sparkles: break out header in layout into Header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Logo from "./Logo.svelte";
+  import { base } from "$app/paths";
+  import AHeader from "./AHeader.svelte";
+</script>
+
+<header>
+  <Logo />
+  <nav aria-label="primary-navigation" class="flex justify-center gap-4">
+    <AHeader href="{base}/">Home</AHeader>
+    <AHeader href="{base}/menu">Menu</AHeader>
+    <AHeader href="{base}/contact">Contact</AHeader>
+    <AHeader href="{base}/reviews">Reviews</AHeader>
+    <AHeader href="{base}/about-us">About us</AHeader>
+  </nav>
+</header>
