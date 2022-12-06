b7e0b436f87e6fe295a65df1ac99b349408ef4cc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-06 22:03:22 +0100

    feat: :sparkles: add menu page with some placeholder data and (now) broken styling

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,9 +1,130 @@
 <script>
-  
+  import P from "$lib/P.svelte";
+  import ButtonA from "$lib/ButtonA.svelte";
+  import MenuItem from "$lib/MenuItem.svelte";
+
+  let menuItems = [
+    {
+      category: "Sweets",
+      items: [
+        {
+          name: "Lorem",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Ipsum",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Dolor",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+      ],
+    },
+    {
+      category: "Drinks",
+      items: [
+        {
+          name: "Lorem",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Ipsum",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Dolor",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+      ],
+    },
+    {
+      category: "Lunch",
+      items: [
+        {
+          name: "Lorem",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Ipsum",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+        {
+          name: "Dolor",
+          src: "/images/.jpg",
+          alt: "",
+          desc: "",
+          price: "20:-",
+        },
+      ],
+    },
+  ];
 </script>
 
-menu
+<main class="flex flex-col items-center justify-between bg-emerald-900">
+  <h2 class="font-thin text-3xl text-emerald-300">Menu</h2>
+
+  <P type="menu">
+    We take a 50 SEK hourly fee (paid in advance) to allow us to care for our
+    animals in the best way. When paying this fee you will also get a feed bag
+    which you can open when you want to give our ducks a treat.
+  </P>
+
+  <P type="menu">
+    Our cafe is very popular so if possible book a time slot to make sure that
+    we will be able to provide the best experience.</P>
+
+  <ButtonA type="book" />
+
+  <div class="alert alert-info shadow-lg m-4">
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="stroke-current flex-shrink-0 w-6 h-6"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+      <span>Swipe down to view our menu.</span>
+    </div>
+  </div>
+
+  {#each menuItems as menuItem}
+    <section>
+      <h2 class="text-3xl m-4 text-emerald-100">{menuItem.category}</h2>
+      {#each menuItem.items as item}
+        <MenuItem {...item} />
+      {/each}
+    </section>
+  {/each}
+</main>
 
 <style>
-  
 </style>
