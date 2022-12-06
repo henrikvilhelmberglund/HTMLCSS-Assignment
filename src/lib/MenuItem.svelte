372e06f8933a9005867a06ce2ee4c5b57077a060
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-06 22:00:30 +0100

    feat: :sparkles: add (now broken) card component for the menu items, TODO: fix styling

---
+++ src/lib/MenuItem.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+  export let src;
+  export let alt;
+  export let desc;
+  export let price;
+</script>
+
+<article class="card lg:card-side bg-base-100 shadow-xl m-2">
+  <figure><img {src} {alt} /></figure>
+  <div class="card-body">
+    <h3 class="card-title">{name}</h3>
+    <p>{desc}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">{price}</button>
+    </div>
+  </div>
+</article>
+
+<style>
+</style>
