8eb6c2abf92b0e065f5f76ef64276e6f62356d40
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-07 17:37:41 +0100

    fix: :art: decrease padding in the motto component

--- src/lib/Motto.svelte
+++ src/lib/Motto.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <p
-  class="w-[100vw] text-3xl bg-orange-400 text-center py-8 border-y border-solid border-black">
+  class="w-[100vw] text-3xl bg-orange-400 text-center py-4 border-y border-solid border-black">
   Out of luck? Pet a duck.
 </p>
 
