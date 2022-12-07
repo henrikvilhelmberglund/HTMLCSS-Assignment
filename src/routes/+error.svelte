1ecf2daa75bd0cc42edd47b4dec2c4973f535048
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-07 18:10:09 +0100

    feat: :art: update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,12 +2,13 @@
   import { page } from "$app/stores";
 </script>
 
-<h1>{$page.status}: {$page.error.message}</h1>
+<h1 class="text-3xl">{$page.status}: {$page.error.message}</h1>
 
+<img src="/images/error.jpg" alt="sad duck" srcset="" />
 {#if $page.status === 404}
-  <p>The duck you're looking for isn't in here.</p>
+  <p class="text-lg">The duck you're looking for isn't in here.</p>
 {:else}
-  <p>Unknown error.</p>
+  <p class="text-lg">Unknown error.</p>
 {/if}
 
 <style>
