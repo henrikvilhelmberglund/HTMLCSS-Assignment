3370d459895ac14501e1b5baed0f727dd46ae00f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-07 17:37:21 +0100

    feat: :sparkles: add a beautiful(?) logo in the logo component

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -2,7 +2,11 @@
   import "@fontsource/cherry-swash";
 </script>
 
-<p class="p-2 font-logo text-4xl text-center bg-gradient-to-b to-orange-50 from-orange-400">Ankademin Cafe</p>
+<!-- <p class="p-2 font-logo text-4xl text-center bg-gradient-to-b to-orange-50 from-orange-400">Ankademin Cafe</p> -->
+<img
+  src="/images/ankademin-cafe/ankademin_cafe_logo.svg"
+  alt="ankademin cafe logo"
+  srcset="" />
 
 <style>
 </style>
