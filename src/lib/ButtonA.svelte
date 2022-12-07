7ddcd5a08543677011b8b54c6abc3d9a0f50adb5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-07 18:12:14 +0100

    fix: :art: update ButtonA component

--- src/lib/ButtonA.svelte
+++ src/lib/ButtonA.svelte
@@ -4,8 +4,13 @@
 </script>
 
 {#if type === "menu"}
-  <a href="{base}/menu/" class="transition-all bg-orange-400 font-thin text-3xl border-2 border-orange-700 rounded-lg p-2
+  <a
+    href="{base}/menu/"
+    class="transition-all bg-orange-400 font-thin text-3xl border-2 border-orange-700 rounded-lg p-2
   hover:bg-orange-300">View menu</a>
 {:else if type === "book"}
-  <a href="{base}/menu/book" class="">Book a time slot</a>
+  <a
+    href="{base}/menu/book"
+    class="transition-all bg-orange-400 font-thin text-3xl border-2 border-orange-700 rounded-lg p-2
+  hover:bg-orange-300">Book a time slot</a>
 {/if}
