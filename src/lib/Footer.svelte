82658e62b50d856d404a247b247934df8873433c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-07 17:36:59 +0100

    feat: :sparkles: add flex wrap to header and footer because of the additional link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,12 +5,13 @@
 
 <footer class="bg-slate-700">
   <p class="p-2 font-logo text-3xl text-center text-white">Ankademin Cafe</p>
-  <nav aria-label="secondary-navigation" class="flex justify-between">
+  <nav aria-label="secondary-navigation" class="flex justify-between flex-wrap">
     <A href="{base}/">Home</A>
     <A href="{base}/menu">Menu</A>
     <A href="{base}/contact">Contact</A>
     <A href="{base}/reviews">Reviews</A>
     <A href="{base}/about-us">About us</A>
+    <A href="{base}/sassy-ducks">Sassy ducks</A>
   </nav>
 
   <small class="text-white text-right block pr-2"
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,11 +6,12 @@
 
 <header>
   <Logo />
-  <nav aria-label="primary-navigation" class="flex justify-between">
+  <nav aria-label="primary-navigation" class="flex justify-between bg-white flex-wrap gap-2">
     <AHeader href="{base}/">Home</AHeader>
     <AHeader href="{base}/menu">Menu</AHeader>
     <AHeader href="{base}/contact">Contact</AHeader>
     <AHeader href="{base}/reviews">Reviews</AHeader>
     <AHeader href="{base}/about-us">About us</AHeader>
+    <AHeader href="{base}/sassy-ducks">Sassy ducks</AHeader>
   </nav>
 </header>
