ff06e2077a81afe619854f29830870cbe2c34d79
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 14:59:58 +0100

    feat: :sparkles: add Review component (WIP)

---
+++ src/lib/Review.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { base } from "$app/paths";
+  export let username;
+  export let score;
+  export let desc;
+</script>
+
+{#each Array(score) as _, i}
+{i}
+  <img src="{base}/images/iconmonstr-star-3.svg" alt="" />
+{/each}
+{desc}
+
+<style>
+</style>
