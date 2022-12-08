a83e411ef06e08ba53accfc4c148b5a5fff42f26
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 15:59:52 +0100

    fix: :bug: fix 404 in employees page

--- src/lib/EmployeesItem.svelte
+++ src/lib/EmployeesItem.svelte
@@ -11,7 +11,7 @@
 <article
   class="card lg:card-side bg-base-100 shadow-xl m-2 bg-emerald-300 flex flex-col rounded">
   <h3 class="card-title text-center text-2xl">{name}</h3>
-  <figure><img {base} {src} {alt} class="w-64" /></figure>
+  <figure><img src="{base}{src}" {alt} class="w-64" /></figure>
   <div class="card-body">
     <div class="card-actions justify-end">
       <p class="">{role}</p>
