a915db0ec81259666cfdd78297930f735caa3d3c
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 16:45:37 +0100

    fix: :bug: debugging

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -5,16 +5,15 @@
 
 <!-- {$page.url.pathname} -->
 <!-- {$page.url.pathname.split("/")} -->
-{$page.url.pathname.split("/")}
 {#each $page.url.pathname.split("/") as url, i}
   {#if i === 0}
     {#if $page.url.pathname === "/"}
       <a href="{base}/{url}">{base}home</a>
     {:else}
-      <a href="{base}/{url}">{base}home/</a>
+      <a href="{base}/{url}/">{base}home/</a>
     {/if}
   {:else if i === 1}
-    <a href="{base}/{url}">{base}{url}/</a>
+    <a href="{base}/{url}/">{base}{url}/</a>
   {:else if i === 2}
     <a href="{base}{url}">{base}{url}/</a>
   {/if}
