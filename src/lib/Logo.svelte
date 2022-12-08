b9ca3791097838c544a4bf7c75ee95f28b7b6cee
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 11:31:23 +0100

    fix: :bug: remove empty srcset attribute (whoops)

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <!-- <p class="p-2 font-logo text-4xl text-center bg-gradient-to-b to-orange-50 from-orange-400">Ankademin Cafe</p> -->
+<!-- src="{base}/images/ankademin-cafe/online.svg" -->
 <img
-  src="{base}/images/ankademin-cafe/ankademin_cafe_logo.svg"
-  alt="ankademin cafe logo"
-  srcset="" />
+  src="{base}/images/ankademin-cafe/ankademin_cafe_logo.jpg"
+  alt="ankademin cafe logo" />
 
 <style>
 </style>
