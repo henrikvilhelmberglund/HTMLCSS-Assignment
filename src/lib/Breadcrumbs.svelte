945afd781ece57cd85cef3adaf91e1edbf43634d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-09 00:00:55 +0100

    fix: :bug: fix breadcrumbs and building (hopefully)

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -1,21 +1,26 @@
 <script>
   import { base } from "$app/paths";
   import { page } from "$app/stores";
+  let second;
 </script>
 
-<!-- {$page.url.pathname} -->
+<!-- {base + "/"} -->
 <!-- {$page.url.pathname.split("/")} -->
-{#each $page.url.pathname.split("/") as url, i}
+{#each $page.route.id.split("/") as url, i}
   {#if i === 0}
-    {#if $page.url.pathname === "/"}
-      <a href="{base}{url}">{base}home</a>
+    {#if $page.route.id.length === 1}
+      <a href="{base}/{url}" class="underline">Home</a>
     {:else}
-      <a href="{base}/{url}">{base}home/</a>
+      <a href="{base}/{url}">Home/</a>
     {/if}
   {:else if i === 1}
-    <a href="{base}/{url}">{base}{url}/</a>
+    {#if $page.route.id.split("/").length !== 3}
+      <a href="{base}/{url}" class="underline">{url}/</a>
+    {:else}
+      <a href="{base}/{url}" class="">{url}/</a>
+    {/if}
   {:else if i === 2}
-    <a href="{base}/{url}">{base}{url}/</a>
+    <a href="{base}{$page.route.id}" class="underline">{url}/</a>
   {/if}
 {/each}
 
