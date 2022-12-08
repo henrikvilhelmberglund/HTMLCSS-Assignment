9e4c0820dfe6a08114d02df0f88b30a7837428fa
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 14:58:17 +0100

    feat: :sparkles: add EmployeesItem component TODO: improve styling

---
+++ src/lib/EmployeesItem.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+  export let src;
+  export let alt;
+  export let role;
+  export let email;
+  export let desc;
+  import { base } from "$app/paths";
+</script>
+
+<article
+  class="card lg:card-side bg-base-100 shadow-xl m-2 bg-emerald-300 flex flex-col rounded">
+  <h3 class="card-title text-center text-2xl">{name}</h3>
+  <figure><img {base} {src} {alt} class="w-64" /></figure>
+  <div class="card-body">
+    <div class="card-actions justify-end">
+      <p class="">{role}</p>
+      <a href="mailto:{email}" class="underline cursor-pointer">{email}</a>
+      <p class="">{desc}</p>
+    </div>
+  </div>
+</article>
+
+<style>
+</style>
