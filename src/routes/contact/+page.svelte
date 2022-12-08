48126cafb2b01e182f414affb89501f059db0802
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-08 16:38:31 +0100

    feat: :sparkles: add contact page (WIP)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,13 @@
 <script>
-  
+  import { base } from "$app/paths";
 </script>
 
-Contact us
+<main>
+  <h1>Contact us</h1>
+  <p>Address:</p>
+  <p>Telefonnummer: 123-4567</p>
+  <img src="{base}/images/ankademin-cafe/ankademin_cafe_entrance.jpg" alt="" />
+</main>
 
 <style>
-  
 </style>
