29c1c9f4daf13451d5b3a13782feb0ec4e866a26
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-09 23:54:56 +0100

    feat(breadcrumbs): :sparkles: put breadcrumbs in nav (secondary-navigation), add some styling for mobile ease of use

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -6,23 +6,40 @@
 
 <!-- {base + "/"} -->
 <!-- {$page.url.pathname.split("/")} -->
-{#each $page.route.id.split("/") as url, i}
-  {#if i === 0}
-    {#if $page.route.id.length === 1}
-      <a href="{base}/{url}" class="underline">Home</a>
-    {:else}
-      <a href="{base}/{url}">Home/</a>
+<nav aria-label="secondary-navigation" class="py-2">
+  {#each $page.route.id.split("/") as url, i}
+    {#if i === 0}
+      {#if $page.route.id.length === 1}
+        <a
+          href="{base}/{url}"
+          class="underline m-1 ml-0 p-1  border border-black rounded bg-emerald-500"
+          >Home</a>
+      {:else}
+        <a href="{base}/{url}" class="m-1 ml-0 p-1 border border-black rounded"
+          >Home</a
+        ><span>/</span>
+      {/if}
+    {:else if i === 1}
+      {#if $page.route.id.split("/").length !== 3}
+        {#if $page.route.id.length !== 1}
+          <a
+            href="{base}/{url}"
+            class="underline m-1 p-1 border border-black rounded bg-emerald-500"
+            >{url}</a>
+        {/if}
+      {:else}
+        <a href="{base}/{url}" class="m-1 p-1 border border-black rounded"
+          >{url}</a
+        ><span>/</span>
+      {/if}
+    {:else if i === 2}
+      <a
+        href="{base}{$page.route.id}"
+        class="underline m-1 p-1 border border-black rounded bg-emerald-500"
+        >{url}</a>
     {/if}
-  {:else if i === 1}
-    {#if $page.route.id.split("/").length !== 3}
-      <a href="{base}/{url}" class="underline">{url}/</a>
-    {:else}
-      <a href="{base}/{url}" class="">{url}/</a>
-    {/if}
-  {:else if i === 2}
-    <a href="{base}{$page.route.id}" class="underline">{url}/</a>
-  {/if}
-{/each}
+  {/each}
+</nav>
 
 <style>
 </style>
