1751be8d35a3ea6535a9a0e7805a78e68596ac5c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-09 23:57:25 +0100

    fix: :art: remove white background space (TODO: should probably have a nice background color instead)

--- src/routes/menu/book/+page.svelte
+++ src/routes/menu/book/+page.svelte
@@ -5,9 +5,11 @@
 
 <main
   in:fly={{ x: -5, duration: 200, delay: 200 }}
-  out:fly={{ x: 5, duration: 200 }}>
+  out:fly={{ x: 5, duration: 200 }}
+  class="h-[50vh] flex flex-col">
   <h2>Booking</h2>
   <P>Call us on 123-4567! Welcome!</P>
+  <div class="bg-emerald-900 flex-1" />
 </main>
 
 <style>
