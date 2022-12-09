deaeb3a29fddc9be4b227e64226493530b59ec76
henrikvilhelmberglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-09 22:59:50 +0000

    Deploying to gh-pages from @ henrikvilhelmberglund/HTMLCSS-Assignment@9494b7d6874dc1330c13c32f23339182dd3a3ec4 🚀

--- _app/immutable/chunks/5-f32eaae0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-ca74f7ee.js";import{default as t}from"../components/pages/contact/_page.svelte-c3c68749.js";export{t as component,r as shared};
---
+++ _app/immutable/chunks/6-4f8e6ec4.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_page-50d61f80.js";import{default as a}from"../components/pages/contact/thank-you/_page.svelte-4711a915.js";const e=!0;export{a as component,e as server,t as shared};
