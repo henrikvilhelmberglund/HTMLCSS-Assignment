60a414eec2b85a2f092af232bca5234dad9370b0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-09 14:57:57 +0100

    feat: :sparkles: add media query component that shows element when media query condition is hit

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,13 +1,29 @@
 <script>
   import "@fontsource/cherry-swash";
   import { base } from "$app/paths";
+  import MediaQuery from "svelte-media-query";
 </script>
 
 <!-- <p class="p-2 font-logo text-4xl text-center bg-gradient-to-b to-orange-50 from-orange-400">Ankademin Cafe</p> -->
 <!-- src="{base}/images/ankademin-cafe/online.svg" -->
-<img
-  src="{base}/images/ankademin-cafe/ankademin_cafe_logo.jpg"
-  alt="ankademin cafe logo" />
+
+<MediaQuery query="(min-width: 480px)" let:matches>
+  {#if matches}
+    <img
+      src="{base}/images/ankademin-cafe/ankademin_cafe_logo_big.webp"
+      width="1536"
+      alt="ankademin cafe logo" />
+  {/if}
+</MediaQuery>
+
+<MediaQuery query="(max-width: 480px)" let:matches>
+  {#if matches}
+    <img
+      src="{base}/images/ankademin-cafe/ankademin_cafe_logo.webp"
+      width="390"
+      alt="ankademin cafe logo" />
+  {/if}
+</MediaQuery>
 
 <style>
 </style>
