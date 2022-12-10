2847b2c03fd70c0154d216a5d52d0522227ebb14
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-10 17:20:15 +0100

    feat: :sparkles: add colors (WIP) and fonts to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const forms = require("@tailwindcss/forms");
+const colors = require("tailwindcss/colors");
 
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -7,6 +8,24 @@ const config = {
     extend: {
       fontFamily: {
         logo: ["Cherry Swash"],
+        h: ["Spartan"],
+        p: ["Nunito"],
+        mat: ["Material Icons Outlined"],
+        matr: ["Material Icons Rounded"],
+      },
+      colors: {
+        anka: {
+          50: colors.slate[300],
+          100: "#f2e8e5",
+          200: "#eaddd7",
+          300: "#e0cec7",
+          400: "#d2bab0",
+          500: "#bfa094",
+          600: "#a18072",
+          700: "#977669",
+          800: "#846358",
+          900: "#43302b",
+        },
       },
     },
   },
