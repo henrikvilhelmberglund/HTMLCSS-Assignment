8052b231ae3b26b7d09a7fb49945a3d29220c338
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-10 17:22:05 +0100

    feat: :sparkles: add dark mode component that toggles dark mode and sets darkStore

---
+++ src/lib/DarkModeButton.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { darkStore } from "./stores.js";
+  let dark = false;
+
+  function toggle() {
+    if (dark) {
+      dark = false;
+      darkStore.set(false);
+    } else if (!dark) {
+      dark = true;
+      darkStore.set(true);
+    }
+  }
+</script>
+
+<button class="border" on:click={toggle}>
+  {#if dark}
+    Toggle Light Mode
+  {:else}
+    Toggle Dark Mode
+  {/if}
+</button>
+
+<style>
+</style>
