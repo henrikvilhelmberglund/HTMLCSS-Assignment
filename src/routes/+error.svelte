b157d5f3679e8cc592e46872977c5a1299571bd8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-10 17:26:49 +0100

    feat: :sparkles: use H component in +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,18 @@
 <script>
   import { page } from "$app/stores";
+  import H from "$lib/H.svelte";
 </script>
 
-<h1 class="text-3xl">{$page.status}: {$page.error.message}</h1>
+<main class="bg-emerald-700">
+  <H type="h1" class="text-3xl">{$page.status}: {$page.error.message}</H>
 
-<img src="/images/error.jpg" alt="sad duck" srcset="" />
-{#if $page.status === 404}
-  <p class="text-lg p-2 italic">The duck you're looking for isn't in here.</p>
-{:else}
-  <p class="text-lg">Unknown error.</p>
-{/if}
+  <img src="/images/error.jpg" alt="sad duck" srcset="" />
+  {#if $page.status === 404}
+    <p class="text-lg p-2 italic">The duck you're looking for isn't in here.</p>
+  {:else}
+    <p class="text-lg">Unknown error.</p>
+  {/if}
+</main>
 
 <style>
 </style>
