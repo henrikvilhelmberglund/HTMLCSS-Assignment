3971e59e2978be6cee4c08fb84a1815e39a7b896
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-11 00:14:00 +0100

    feat(components): :sparkles: add shared classes in variable, update styling

--- src/lib/P.svelte
+++ src/lib/P.svelte
@@ -1,27 +1,28 @@
 <script>
   export let type;
   export let _class = "";
-  let shared = "font-p";
+  let shared =
+    "font-p bg-emerald-800 text-emerald-200 border-b border-solid border-black";
 </script>
 
 {#if type === "index"}
-  <p
-    class="p-2 bg-emerald-800 text-emerald-200 border-b border-solid border-black text-center {shared} {_class}">
+  <p class="p-2 text-center {shared} {_class}">
     <slot />
   </p>
 {:else if type === "menu"}
-  <p
-    class="p-2  bg-emerald-800 text-emerald-200 border-b border-solid border-black text-center {shared} {_class}">
+  <p class="p-2 text-center {shared} {_class}">
     <slot />
   </p>
 {:else if type === "history"}
-  <p
-    class="p-2  bg-emerald-800 text-emerald-200 border-b border-solid border-black {shared} {_class}">
+  <p class="p-2 {shared} {_class}">
+    <slot />
+  </p>
+{:else if type === "motto"}
+  <p class="p-2 font-h font-bold {_class}">
     <slot />
   </p>
 {:else}
-  <p
-    class="p-2  bg-emerald-800 text-emerald-200 border-b border-solid border-black text-center {shared} {_class}">
+  <p class="p-2 {_class}">
     <slot />
   </p>
 {/if}
