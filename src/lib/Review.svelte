ef30f04138ebf0cbce9314fc39da6880caedb747
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-11 00:15:16 +0100

    feat(components): :sparkles: use P component in Review.svelte

--- src/lib/Review.svelte
+++ src/lib/Review.svelte
@@ -3,12 +3,13 @@
   export let username;
   export let score;
   export let desc;
+  import P from "./P.svelte";
 </script>
 
 <article class="flex flex-col border-t border-solid border-black p-4">
-  <p class="text-xl">{username}</p>
+  <P type="review" class="text-xl">{username}</P>
   <section class="flex flex-row">
-    {#each Array(score+1) as _, i}
+    {#each Array(score + 1) as _, i}
       {#if i !== 0 && i % 2 === 0}
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -29,9 +30,9 @@
           d="m12 .587 3.668 7.568L24 9.306l-6.064 5.828 1.48 8.279L12 19.446l-7.417 3.967 1.481-8.279L0 9.306l8.332-1.151z" /></svg>
     {/if}
   </section>
-  <p class="pt-4">
+  <P type="review" class="pt-4">
     {desc}
-  </p>
+  </P>
 </article>
 
 <style>
