5999917f46478a35e01eb75d8447cd1d7d538cde
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-11 18:07:05 +0100

    feat: :sparkles: move dark mode to breadcrumbs + prettier formatting

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -1,6 +1,8 @@
 <script>
-  import { base } from "$app/paths";
-  import { page } from "$app/stores";
+import { base } from "$app/paths";
+import { page } from "$app/stores";
+import DarkModeButton from "./DarkModeButton.svelte";
+import { darkStore } from "./stores.js";
 </script>
 
 <!-- {base + "/"} -->
@@ -12,34 +14,37 @@
         {#if $page.route.id.length === 1}
           <a
             href="{base}/{url}"
-            class="underline m-1 ml-0 p-1  border border-black rounded bg-emerald-500"
-            >Home</a>
+            class="m-1 ml-0  rounded border border-black bg-emerald-500 p-1 underline">
+            Home</a>
         {:else}
           <a
             href="{base}/{url}"
-            class="m-1 ml-0 p-1 border border-black rounded">Home</a
-          ><span>/</span>
+            class="m-1 ml-0 rounded border border-black p-1">Home</a>
+          <span>/</span>
         {/if}
       {:else if i === 1}
         {#if $page.route.id.split("/").length !== 3}
           {#if $page.route.id.length !== 1}
             <a
               href="{base}/{url}"
-              class="underline m-1 p-1 border border-black rounded bg-emerald-500"
-              >{url}</a>
+              class="m-1 rounded border border-black bg-emerald-500 p-1 underline">
+              {url}</a>
           {/if}
         {:else}
-          <a href="{base}/{url}" class="m-1 p-1 border border-black rounded"
-            >{url}</a
-          ><span>/</span>
+          <a href="{base}/{url}" class="m-1 rounded border border-black p-1">
+            {url}</a>
+          <span>/</span>
         {/if}
       {:else if i === 2}
         <a
           href="{base}{$page.route.id}"
-          class="underline m-1 p-1 border border-black rounded bg-emerald-500"
-          >{url}</a>
+          class="m-1 rounded border border-black bg-emerald-500 p-1 underline">
+          {url}</a>
       {/if}
     {/each}
+    <div class="relative">
+      <DarkModeButton _class="absolute right-0 -bottom-2" />
+    </div>
   </nav>
 {/if}
 
