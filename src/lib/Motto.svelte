0cbbe878b4dc3993f9fc2de7f3bae06620ce9ced
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-11 20:48:53 +0100

    fix(components): :bug: 100vw creates a horizontal scrollbar so use w-full (100%) instead

--- src/lib/Motto.svelte
+++ src/lib/Motto.svelte
@@ -4,7 +4,7 @@ import P from "./P.svelte";
 
 <P
   type="motto"
-  _class="w-[100vw] text-3xl bg-orange-400 text-center py-4 border-y border-solid border-black">
+  _class="w-full text-3xl bg-orange-400 text-center py-4 border-y border-solid border-black">
   Out of luck? Pet a duck. 🍰🦆☕
 </P>
 
