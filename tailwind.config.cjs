50ff046dc62a7fb00ca7a0e6b033b604e47d89ee
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-11 23:31:14 +0100

    fix: :fire: remove own color system which was going to confuse me anyway

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,20 +13,6 @@ const config = {
         mat: ["Material Icons Outlined"],
         matr: ["Material Icons Rounded"],
       },
-      colors: {
-        anka: {
-          50: colors.slate[300],
-          100: "#f2e8e5",
-          200: "#eaddd7",
-          300: "#e0cec7",
-          400: "#d2bab0",
-          500: "#bfa094",
-          600: "#a18072",
-          700: "#977669",
-          800: "#846358",
-          900: "#43302b",
-        },
-      },
     },
   },
 
