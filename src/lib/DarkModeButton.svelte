557ff36fb21e05075fa0927f1a3175116be17f94
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-12 19:04:57 +0100

    fix(components): :bug: change md: to lg: because it was a bit janky (covered the nav)

--- src/lib/DarkModeButton.svelte
+++ src/lib/DarkModeButton.svelte
@@ -29,13 +29,13 @@ if (browser) {
 </script>
 
 <button
-  class="mb-1 h-8 w-16 rounded-full border-2 border-solid border-black bg-slate-100 dark:border-white dark:bg-slate-800 md:mb-28 md:mr-6 md:h-36 md:w-36 md:rounded-lg {_class}"
+  class="mb-1 h-8 w-16 rounded-full border-2 border-solid border-black bg-slate-100 dark:border-white dark:bg-slate-800 lg:mb-28 md:mr-6 lg:h-36 lg:w-36 lg:rounded-lg {_class}"
   on:click={toggle}>
   {#if dark}
-    <span class="font-matr text-2xl text-yellow-300 md:text-8xl"
+    <span class="font-matr text-2xl text-yellow-300 lg:text-8xl"
       >light_mode</span>
   {:else}
-    <span class="font-matr text-2xl text-black md:text-8xl">dark_mode</span>
+    <span class="font-matr text-2xl text-black lg:text-8xl">dark_mode</span>
   {/if}
 </button>
 
