1e520179b7cf6e4c7b308118049f4cd7aea52264
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 15:34:38 +0100

    fix(components): :lipstick: in case of extra styles

--- src/lib/StarButton.svelte
+++ src/lib/StarButton.svelte
@@ -23,7 +23,7 @@ export let score = 0;
             xmlns="http://www.w3.org/2000/svg"
             width="12"
             height="24"
-            class={score >= i ? "fill-orange-400" : "fill-slate-400"}>
+            class="{score >= i ? 'fill-orange-400' : 'fill-slate-400'} ">
             <path
               d="m12 .587 3.668 7.568L24 9.306l-6.064 5.828 1.48 8.279L12 19.446l-7.417 3.967 1.481-8.279L0 9.306l8.332-1.151z" /></svg>
         </button>
