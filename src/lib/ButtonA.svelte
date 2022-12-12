46e044596a1a2f4e50def414b1f5971bf44b5a06
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-12 08:01:38 +0100

    feat(components): :lipstick: add m-2 ButtonA.svelte

--- src/lib/ButtonA.svelte
+++ src/lib/ButtonA.svelte
@@ -6,16 +6,16 @@ export let type;
 {#if type === "menu"}
   <a
     href="{base}/menu/"
-    class="rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
+    class="m-2 rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
   hover:bg-orange-300">View menu</a>
 {:else if type === "book"}
   <a
     href="{base}/menu/book"
-    class="rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
+    class="m-2 rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
   hover:bg-orange-300">Book a time slot</a>
 {:else if type === "employees"}
   <a
     href="{base}/about-us/employees"
-    class="rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
+    class="m-2 rounded-lg border-2 border-orange-700 bg-orange-400 p-2 text-3xl font-thin transition-all
   hover:bg-orange-300">Employees page</a>
 {/if}
