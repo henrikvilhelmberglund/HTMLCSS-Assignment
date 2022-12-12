dbe7ff647b4832306200134cb22fead6643b3f61
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 15:47:51 +0100

    feat: :sparkles: use fancy local storage store for custom user reviews

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
+import { writable as localstore } from "svelte-local-storage-store";
 
 export const darkStore = writable(false);
 export const menuClicked = writable(false);
-export const userReview = writable([]);
+export const userReview = localstore("Ankademin Cafe Reviews", []);
