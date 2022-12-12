1a12cb91aff1a50e0ad4fc3cbeb121862ac06951
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 10:49:57 +0100

    feat(components): :sparkles: less janky way of having a backdrop 🥇

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,5 +1,6 @@
 <script>
 import { base } from "$app/paths";
+import { fade } from "svelte/transition";
 export let type;
 export let src;
 export let alt;
@@ -19,18 +20,37 @@ import { menuClicked } from "./stores.js";
         menuClicked.set(false);
       }}
       class="relative right-16 top-14 flex-1">
+      <div
+        class="fixed top-0 right-0 left-0 bottom-0 h-[100vh] w-[100vw] md:fixed md:bottom-0 md:top-0 md:right-0 md:left-0 md:h-[100vh] md:w-[100vw]  {$menuClicked
+          ? 'backdrop-blur-lg backdrop-brightness-50'
+          : ''}" />
+    </div>
+  {/if}
+  {#if clicked}
+    <div
+      in:fade={{ duration: 300 }}
+      out:fade={{ duration: 100 }}
+      on:click={() => {
+        clicked = false;
+        menuClicked.set(false);
+      }}
+      on:keydown={() => {
+        clicked = false;
+        menuClicked.set(false);
+      }}
+      class="relative right-16 top-14 flex-1">
       <img
+        on:click={() => {
+          clicked = false;
+          menuClicked.set(false);
+        }}
+        on:keydown={() => {
+          clicked = false;
+          menuClicked.set(false);
+        }}
         {src}
         {alt}
-        class="z-100 absolute bottom-0 m-0 min-w-[96vw] p-0 filter-none md:min-w-[70vw]" />
-      <div
-        class="absolute top-0 h-[100vh] w-[96vw] {$menuClicked
-          ? 'backdrop-blur-lg backdrop-brightness-50'
-          : ''}" />
-      <div
-        class="absolute bottom-[43vh] h-[100vh] w-[96vw] {$menuClicked
-          ? 'backdrop-blur-lg backdrop-brightness-50'
-          : ''}" />
+        class="z-100 absolute bottom-0 m-0 min-w-[100vw] p-0 md:-bottom-32 md:min-w-[45vw]" />
     </div>
   {/if}
   <button
