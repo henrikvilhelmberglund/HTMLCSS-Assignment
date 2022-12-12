0d378ed4f9791b0cdd10828b5443abbf4d2d1455
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 11:47:30 +0100

    feat(darkmode): :lipstick: update dark mode in Breadcrumbs.svelte

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -10,18 +10,18 @@ import { darkStore } from "./stores.js";
 {#if $page.route.id !== null}
   <nav
     aria-label="secondary-navigation"
-    class="bg-gradient-to-b from-sky-200 to-emerald-200 py-2 dark:bg-emerald-800 dark:from-sky-900 dark:to-emerald-800">
+    class="bg-gradient-to-b from-sky-200 to-emerald-200 py-2 text-black dark:bg-emerald-800 dark:from-sky-900 dark:to-emerald-800 dark:text-white">
     {#each $page.route.id.split("/") as url, i}
       {#if i === 0}
         {#if $page.route.id.length === 1}
           <a
             href="{base}/{url}"
-            class="m-1 ml-0  rounded border border-black bg-emerald-500 p-1 underline">
+            class="m-1 ml-0  rounded border border-black bg-emerald-500 p-1 underline dark:text-black">
             Home</a>
         {:else}
           <a
             href="{base}/{url}"
-            class="m-1 ml-0 rounded border border-black p-1">Home</a>
+            class="m-1 ml-0 rounded border border-black p-1 ">Home</a>
           <span>/</span>
         {/if}
       {:else if i === 1}
@@ -29,7 +29,7 @@ import { darkStore } from "./stores.js";
           {#if $page.route.id.length !== 1}
             <a
               href="{base}/{url}"
-              class="m-1 rounded border border-black bg-emerald-500 p-1 underline">
+              class="m-1 rounded border border-black bg-emerald-500 p-1 underline dark:text-black">
               {url}</a>
           {/if}
         {:else}
@@ -40,7 +40,7 @@ import { darkStore } from "./stores.js";
       {:else if i === 2}
         <a
           href="{base}{$page.route.id}"
-          class="m-1 rounded border border-black bg-emerald-500 p-1 underline">
+          class="m-1 rounded border border-black bg-emerald-500 p-1 underline dark:text-black">
           {url}</a>
       {/if}
     {/each}
