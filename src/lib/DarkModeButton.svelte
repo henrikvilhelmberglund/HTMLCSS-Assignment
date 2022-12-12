e188923a57da2ae1850ad52d7e157769cf8d8b60
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 15:17:53 +0100

    feat: :sparkles: add responsivity to dark mode button

--- src/lib/DarkModeButton.svelte
+++ src/lib/DarkModeButton.svelte
@@ -29,12 +29,12 @@ if (browser) {
 </script>
 
 <button
-  class="mb-1 h-8 w-16 rounded-full border-2 border-solid border-black bg-slate-100 dark:border-white dark:bg-slate-800 {_class}"
+  class="mb-1 h-8 w-16 rounded-full border-2 border-solid border-black bg-slate-100 dark:border-white dark:bg-slate-800 md:mb-28 md:mr-6 md:h-36 md:w-36 md:rounded-lg {_class}"
   on:click={toggle}>
   {#if dark}
-    <p class="font-matr text-2xl text-yellow-300">light_mode</p>
+    <p class="font-matr text-2xl text-yellow-300 md:text-8xl">light_mode</p>
   {:else}
-    <p class="font-matr text-2xl text-black">dark_mode</p>
+    <p class="font-matr text-2xl text-black md:text-8xl">dark_mode</p>
   {/if}
 </button>
 
