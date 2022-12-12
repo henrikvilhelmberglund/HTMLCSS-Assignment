5e6dade238e95fe0b49629f2a6de8a4fd071a1bb
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-12 11:49:17 +0100

    feat(footer): :lipstick: update Footer styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,24 +3,25 @@ import A from "$lib/A.svelte";
 import { base } from "$app/paths";
 </script>
 
-<footer>
+<footer class="bg-slate-400 dark:bg-slate-700">
   <p
     class="bg-gradient-to-b from-emerald-200 via-slate-300 to-slate-400 p-2 text-center font-logo text-3xl text-black dark:from-emerald-800 dark:via-slate-600 dark:to-slate-700 dark:text-white">
     Ankademin Cafe
   </p>
   <nav
     aria-label="primary-navigation"
-    class="flex flex-wrap justify-between  bg-slate-400 dark:bg-slate-700">
+    class="flex h-32 w-full flex-row flex-wrap justify-between">
     <A href="{base}/">Home</A>
     <A href="{base}/menu">Menu</A>
     <A href="{base}/about-us">About us</A>
     <A href="{base}/reviews">Reviews</A>
     <A href="{base}/contact">Contact</A>
     <A href="{base}/sassy-ducks">Sassy ducks</A>
-    <small class="block pr-2 text-right text-white"
-      >&copy; Copyright 2022 Ankademin Cafe</small>
   </nav>
-
+  <div class="relative">
+    <small class="absolute bottom-0 right-0 pb-2 pr-2 text-right text-white">
+      &copy; Copyright 2022 Ankademin Cafe</small>
+  </div>
 </footer>
 
 <style>
