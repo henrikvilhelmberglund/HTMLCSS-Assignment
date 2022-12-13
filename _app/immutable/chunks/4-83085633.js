f546f5870462c6a9b9ca5eac934c2755035967c6
henrikvilhelmberglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-13 10:53:38 +0000

    Deploying to gh-pages from @ henrikvilhelmberglund/HTMLCSS-Assignment@af2fa60e8535a979e23e797a134a79839c9c7474 🚀

--- _app/immutable/chunks/4-83085633.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-d1384927.js";import{default as t}from"../components/pages/about-us/employees/_page.svelte-41cd990b.js";export{t as component,r as shared};
---
+++ _app/immutable/chunks/6-76c20866.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_page-316df81a.js";import{default as a}from"../components/pages/contact/thank-you/_page.svelte-989376e4.js";const e=!0;export{a as component,e as server,t as shared};
