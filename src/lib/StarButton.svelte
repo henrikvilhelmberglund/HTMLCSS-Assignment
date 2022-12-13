a69bf8f62460a39376c472e5c233cb27164c730f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-13 14:44:30 +0100

    feat(components): :sparkles: add Star class to remove transition on click, change on:click to on:mousedown for snappier UI

--- src/lib/StarButton.svelte
+++ src/lib/StarButton.svelte
@@ -8,13 +8,13 @@ export let score = 0;
       {#if i % 2 === 0}
         <button
           aria-label={i / 2 === 1 ? i / 2 + " star" : i / 2 + " stars"}
-          on:click={() => (score = i)}>
+          on:mousedown={() => (score = i)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="12"
             height="24"
             class="{score >= i ? 'fill-orange-400' : 'fill-slate-400'}
-            -scale-x-100">
+            star -scale-x-100">
             <path
               d="m12 .587 3.668 7.568L24 9.306l-6.064 5.828 1.48 8.279L12 19.446l-7.417 3.967 1.481-8.279L0 9.306l8.332-1.151z" /></svg>
         </button>
@@ -22,12 +22,12 @@ export let score = 0;
       {#if i % 2 === 1}
         <button
           aria-label={i / 2 === 0.5 ? i / 2 + " star" : i / 2 + " stars"}
-          on:click={() => (score = i)}>
+          on:mousedown={() => (score = i)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="12"
             height="24"
-            class="{score >= i ? 'fill-orange-400' : 'fill-slate-400'} ">
+            class="{score >= i ? 'fill-orange-400' : 'fill-slate-400'} star ">
             <path
               d="m12 .587 3.668 7.568L24 9.306l-6.064 5.828 1.48 8.279L12 19.446l-7.417 3.967 1.481-8.279L0 9.306l8.332-1.151z" /></svg>
         </button>
