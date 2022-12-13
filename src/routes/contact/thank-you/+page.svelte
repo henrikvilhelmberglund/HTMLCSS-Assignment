803ebc77a597c7de0945783eeaf681728ce8bac5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-13 14:45:24 +0100

    fix: :iphone: slightly better desktop mode for thank you page

--- src/routes/contact/thank-you/+page.svelte
+++ src/routes/contact/thank-you/+page.svelte
@@ -4,7 +4,7 @@ import P from "$lib/P.svelte";
 import H from "$lib/H.svelte";
 </script>
 
-<main class="flex w-[100vw] h-[45vh] flex-col break-words">
+<main class="flex w-[100vw] h-[45vh] md:h-[54vh] flex-col break-words">
   {#if form.textarea}
     <H type="h1"
       >Thank you for your feedback, {form.email || "unknown person"}!</H>
