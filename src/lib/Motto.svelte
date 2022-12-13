77e4cea1193742a4f5d34b517e1f3d2a9e3195b0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-12-13 14:44:02 +0100

    fix(components): :iphone: make Motto look better on desktop

--- src/lib/Motto.svelte
+++ src/lib/Motto.svelte
@@ -4,7 +4,7 @@ import P from "./P.svelte";
 
 <P
   type="motto"
-  _class="w-full text-3xl bg-orange-400 text-center py-4 border-y border-solid border-black">
+  _class="w-full text-3xl md:text-5xl bg-orange-400 text-center !py-4 border-y border-solid border-black md:border md:rounded-xl md:m-8">
   Out of luck? Pet a duck. 🍰🦆☕
 </P>
 
